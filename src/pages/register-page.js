73d8335e67af7c74d1f613adbd78e6c7fee818e2
kvelraj_concord <kvelraj@concord.net>
2021-06-29 08:26:05 +0530

    registerpage

--- src/pages/register-page.js
+++ src/pages/register-page.js
@@ -1,36 +1,61 @@
 import React, { Component } from 'react';
 
 class RegisterPage extends Component{
- 
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      firstName : '',
+      lastName : '',
+      email : '',
+      password  : '',
+      dateOfBirth : '',
+      gender : '',
+      hobbies : [],
+      address : '',
+      city : ''
+    }
+  }
+
+  onHandleInput = (event) => {
+    this.setState({
+      [event.target.name] : event.target.value
+    })
+  }
+
+  createAccount(){
+    console.log(this.state)
+  }
+
   render() {
     return (
       <div className="login-form">
           <h2>Create your Account</h2>
           <div>
             <label className="label">Enter your First Name </label>
-            <input type="text" className="input" placeholder="Please enter first name.."/>
+            <input type="text" className="input" name="firstName" placeholder="Please enter first name.." onChange={this.onHandleInput}/>
           </div>
           <div className="m-top-15">
             <label className="label">Enter your Last Name </label>
-            <input type="text" className="input" placeholder="Please enter last name.."/>
+            <input type="text" className="input" name="lastName" placeholder="Please enter last name.." onChange={this.onHandleInput}/>
           </div>
           <div className="m-top-15">
             <label className="label">Enter your Email Id </label>
-            <input type="text" className="input" placeholder="Please enter email id.."/>
+            <input type="text" className="input" name="email" placeholder="Please enter email id.." onChange={this.onHandleInput}/>
           </div>
           <div className="m-top-15">
             <label className="label">Enter your Password </label>
-            <input type="text" className="input" placeholder="Please enter password.."/>
+            <input type="text" className="input" name="password" placeholder="Please enter password.." onChange={this.onHandleInput}/>
           </div>
           <div className="m-top-15">
             <label className="label">Select your D.O.B </label>
-            <input type="date" className="input"/>
+            <input type="date" className="input" name="dateOfBirth" onChange={this.onHandleInput}/>
           </div>
           <div className="m-top-15">
             <label className="label">Select your Gender </label>
-            <input type="radio" name="gender" />Male
-            <input type="radio" name="gender"/>Female
-            <input type="radio" name="gender"/>Others
+            <input type="radio" name="gender" value="male" onChange={this.onHandleInput}/>Male
+            <input type="radio" name="gender" value="female" onChange={this.onHandleInput}/>Female
+            <input type="radio" name="gender" value="do now wish to specify" onChange={this.onHandleInput}/>Others
           </div>
           <div className="m-top-15">
             <label className="label">Enter your Hobbies </label>
@@ -42,23 +67,23 @@ class RegisterPage extends Component{
           </div>
           <div className="m-top-15">
             <label className="label">Enter your Address</label>
-            <textarea placeholder="Please enter address" className="text-box"></textarea>
+            <textarea placeholder="Please enter address" name="address" className="text-box" onChange={this.onHandleInput}></textarea>
           </div>
           <div className="m-top-15">
             <label className="label">Select your City</label>
-            <select className="input" defaultValue="">
+            <select className="input" defaultValue="" onChange={this.onHandleInput} name="city">
               <option value="" disabled>Please select any City</option>
-              <option>Chennai</option>
-              <option>Mumbai</option>
-              <option>Hydrabad</option>
-              <option>Cochin</option>
-              <option>Delhi</option>
-              <option>Banglore</option>
-              <option>Kolakatha</option>
+              <option value="CH">Chennai</option>
+              <option value="BOM">Mumbai</option>
+              <option value="HYD">Hydrabad</option>
+              <option value="KO">Cochin</option>
+              <option value="DH">Delhi</option>
+              <option value="BLORE">Banglore</option>
+              <option value="KOL">Kolakatha</option>
             </select>
           </div>
           <div className="m-top-15">
-            <button>Create My Account</button>
+            <button onClick={() => { this.createAccount(); }}>Create My Account</button>
           </div>
       </div>
     );
