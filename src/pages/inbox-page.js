61abb6b1e18ad4e94ce4a963aa5328eb9b30248b
kvelraj_concord <kvelraj@concord.net>
2021-07-12 08:32:25 +0530

    api

--- src/pages/inbox-page.js
+++ src/pages/inbox-page.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class InboxPage extends Component {
+  
+  listAPI(){
+    
+    const url = "https://reqres.in/api/users?page=2";
+    axios.get(url)
+        .then((response) => {
+          console.log(response)
+        })
+        .catch((error) => {
+          console.log(error)
+        });
+
+  }
+
   render() {
     return (
       <div>
         <h1>This is a INBOX PAGE</h1>
+        <button onClick={() => this.listAPI()}>List API</button>
       </div>
     );
   }
