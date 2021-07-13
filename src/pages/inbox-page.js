581c120845dbacc28587200b51ff19b707d0a675
kvelraj_concord <kvelraj@concord.net>
2021-07-13 08:00:06 +0530

    map

--- src/pages/inbox-page.js
+++ src/pages/inbox-page.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class InboxPage extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      userList : []
+    }
+  }
   
   listAPI(){
     
     const url = "https://reqres.in/api/users?page=2";
     axios.get(url)
         .then((response) => {
-          console.log(response)
+          console.log(response.data.data);
+          this.setState({
+            userList : response.data.data
+          })
         })
         .catch((error) => {
           console.log(error)
@@ -17,10 +27,40 @@ class InboxPage extends Component {
   }
 
   render() {
+    let users = this.state.userList.map((value, index) => {
+      return(
+        <tr key={index}>
+          <td>{value.id}</td>
+          <td>{value.first_name}</td>
+          <td>{value.last_name}</td>
+          <td>{value.email}</td>
+          <td>
+            <img src={value.avatar} />
+          </td>
+        </tr>
+      )
+    });
+
+    console.log(users)
+
     return (
       <div>
         <h1>This is a INBOX PAGE</h1>
         <button onClick={() => this.listAPI()}>List API</button>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Email ID</th>
+              <th>Profile Picture</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users}
+          </tbody>
+        </table>
       </div>
     );
   }
