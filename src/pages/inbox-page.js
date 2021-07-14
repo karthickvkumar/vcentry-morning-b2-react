976b39fa290e9f465ae6c98ef1c61ce98757a760
kvelraj_concord <kvelraj@concord.net>
2021-07-14 07:59:03 +0530

    put

--- src/pages/inbox-page.js
+++ src/pages/inbox-page.js
@@ -30,8 +30,8 @@ class InboxPage extends Component {
     let users = this.state.userList.map((value, index) => {
       return(
         <tr key={index}>
-          <td>{value.first_name}</td>
           <td>{value.id}</td>
+          <td>{value.first_name}</td>
           <td>{value.last_name}</td>
           <td>{value.email}</td>
           <td>
--- src/pages/trash-page.js
+++ src/pages/trash-page.js
@@ -1,10 +1,54 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class TrashPage extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      edit : {
+        name : '',
+        job : ''
+      }
+    }
+  }
+
+  onHandleInput = (event) => {
+    this.setState({
+      edit : {...this.state.edit, [event.target.name] : event.target.value}
+    })
+  }
+
+  onUpdate(){
+    console.log(this.state.edit)
+
+    const url = "https://reqres.in/api/users/2";
+
+    axios.put(url, this.state.edit)
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        })
+
+  }
+
   render() {
     return (
       <div>
         <h1>This is a TRASH PAGE</h1>
+        <div>
+          <label>Edit Username :</label>
+          <input type="text" name="name" placeholder="Edit username.." onChange={this.onHandleInput}/>
+        </div>
+        <div>
+          <label>Edit Job :</label>
+          <input type="text" name="job" placeholder="Edit job.." onChange={this.onHandleInput}/>
+        </div>
+        <div>
+          <button onClick={() => this.onUpdate()}>Update Info</button>
+        </div>
       </div>
     );
   }
